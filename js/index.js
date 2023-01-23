f977582a0d979fefe09309447dfdf71120e89d46
ZeroDawn0D <umang26112003@gmail.com>
2023-01-23 16:01:05 +0530

    more team

--- js/index.js
+++ js/index.js
@@ -15,3 +15,31 @@ dropdownButton = document.getElementsByClassName("mobile")[0];
 dropdown = document.getElementsByClassName("drop-down")[0];
 dropdown.style.maxHeight = "0px"
 dropdownButton.onclick = changeMenu
+
+
+
+changeTeamDropdown = function(){
+	parent = this.parentElement;
+	dropdownText = parent.getElementsByClassName("drop-down")[0];
+	
+	if(dropdownText.style.maxHeight == "200px"){
+		dropdownText.style.maxHeight = "0px"
+	}else{
+		dropdownText.style.maxHeight = "200px"
+	}
+	
+}
+
+
+team = document.getElementById("team")
+
+teamDropdowns = team.getElementsByClassName("drop-down")
+for(i = 0; i < teamDropdowns.length; i++){
+	teamDropdowns[i].style.maxHeight = "0px"
+}
+
+teamDropdownButtons = team.getElementsByClassName("drop-down-button")
+
+for(i = 0; i < teamDropdownButtons.length; i++){
+	teamDropdownButtons[i].onclick = changeTeamDropdown
+}
